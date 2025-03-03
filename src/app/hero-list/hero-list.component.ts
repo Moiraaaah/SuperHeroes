ee952614215a62b7c698676b91c2d855e6b95ec1
moperrot <moira.perrot@etudiant.univ-rennes1.fr>
2025-03-03 09:27:17 +0100

    Barre de recherche

--- src/app/hero-list/hero-list.component.ts
+++ src/app/hero-list/hero-list.component.ts
@@ -1,23 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroService } from '../hero.service';
-import {NgForOf} from '@angular/common';
+import { NgForOf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-list',
   templateUrl: './hero-list.component.html',
   imports: [
-    NgForOf
+    NgForOf,
+    FormsModule
   ],
   styleUrls: ['./hero-list.component.css']
 })
 export class HeroListComponent implements OnInit {
   heroes: any[] = [];
+  filteredHeroes: any[] = [];
+  search: string = '';
 
   constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.heroService.getHeroes().subscribe((data) => {
       this.heroes = data;
+      this.filteredHeroes = data;
     });
+  }
+
+  filterHeroes(): void {
+    this.filteredHeroes = this.heroes.filter(hero =>
+      hero.name.toLowerCase().includes(this.search.toLowerCase()) //pas respect de casse
+    );
   }
 }
